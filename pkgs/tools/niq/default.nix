1b9827198f63027bbe817006929f0235f7f20da5
Wietse de Vries <wietsedvries@gmail.com>
2020-08-01 11:03:42 +0200

    Fix niq versioning

--- pkgs/tools/niq/default.nix
+++ pkgs/tools/niq/default.nix
@@ -1,19 +1,17 @@
 { pkgs, lib, fetchFromGitHub, python38Packages }:
 
-with python38Packages;
-
-buildPythonApplication rec {
-  name = "niq";
-  version = "2020-08-01";
+python38Packages.buildPythonApplication rec {
+  pname = "niq";
+  version = "0.0.1";
 
   src = fetchFromGitHub {
       owner = "wietsedv";
       repo = "nix-niq";
-      rev = "0c22d76d469630cf7aa7e027b1c56ddd9ed683ad";
-      sha256 = "0rw9xzvdmxfmvnsw12hmx3gs3znra2ds61bkf420d3i7dpl6412h";
+      rev = "000006e1c1d98c39ab7483dbcf5c94688ef5e49e";
+      sha256 = "1f5m8bl03gn8y1ibzpyksdn8wn9fhnqqavd5pf4xgp2a9j2xbi8p";
   };
 
-  propagatedBuildInputs = [
+  propagatedBuildInputs = with python38Packages; [
     requests
     brotli
   ];
